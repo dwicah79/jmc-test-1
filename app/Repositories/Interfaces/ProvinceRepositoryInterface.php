07e67fa7c4a87c93b70db9c758c3182d1dc4eb38
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 01:55:07 +0700

    hotfix: add function report province population and sorted

--- app/Repositories/Interfaces/ProvinceRepositoryInterface.php
+++ app/Repositories/Interfaces/ProvinceRepositoryInterface.php
@@ -9,4 +9,6 @@ interface ProvinceRepositoryInterface
     public function find($id);
     public function update($id, array $data);
     public function delete($id);
+    public function getAllWithPopulationSum();
+    public function getAllSorted();
 }
