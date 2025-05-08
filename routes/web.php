f5b44fef06721d3c77241c0e77dc8039b14195df
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 01:06:10 +0700

    create route for regency

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProvinceController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegencyController;
+use App\Http\Controllers\ProvinceController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,4 +14,12 @@ Route::prefix('provinces')->group(function () {
     Route::get('/{province}/edit', [ProvinceController::class, 'edit'])->name('provinces.edit');
     Route::put('/{province}', [ProvinceController::class, 'update'])->name('provinces.update');
     Route::delete('/{province}', [ProvinceController::class, 'destroy'])->name('provinces.destroy');
+});
+
+route::prefix('regencies')->group(function () {
+    Route::get('/', [RegencyController::class, 'index'])->name('regencies.index');
+    Route::post('/', [RegencyController::class, 'store'])->name('regencies.store');
+    Route::get('/{regency}/edit', [RegencyController::class, 'edit'])->name('regencies.edit');
+    Route::put('/{regency}', [RegencyController::class, 'update'])->name('regencies.update');
+    Route::delete('/{regency}', [RegencyController::class, 'destroy'])->name('regencies.destroy');
 });
