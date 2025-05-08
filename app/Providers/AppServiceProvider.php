58531ac6922465d729fe64aaa91ea07ca7a86057
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-08 15:31:22 +0700

    register repositorios in provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\Interfaces\ProvinceRepositoryInterface;
+use App\Repositories\Interfaces\RegencyRepositoryInterface;
+use App\Repositories\ProvinceRepository;
+use App\Repositories\RegencyRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ProvinceRepositoryInterface::class, ProvinceRepository::class);
+        $this->app->bind(RegencyRepositoryInterface::class, RegencyRepository::class);
     }
 
     /**
