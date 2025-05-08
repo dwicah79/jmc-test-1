6cb075137d6984f11a81a92c425ce46ac1dcf902
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 00:50:31 +0700

    create index function in regency controller

---
+++ app/Http/Controllers/RegencyController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Regency;
+use App\Models\Province;
+use Illuminate\Http\Request;
+use App\Repositories\Interfaces\RegencyRepositoryInterface;
+
+class RegencyController extends Controller
+{
+    protected $regencyRepository;
+
+    public function __construct(RegencyRepositoryInterface $regencyRepository)
+    {
+        $this->regencyRepository = $regencyRepository;
+    }
+
+    public function index(Request $request)
+    {
+        $search = $request->input('search');
+        $provinceId = $request->input('province_id');
+
+        $query = Regency::with('province')
+            ->when($provinceId, function ($q) use ($provinceId) {
+                return $q->where('province_id', $provinceId);
+            })
+            ->when($search, function ($q) use ($search) {
+                return $q->where(function ($q) use ($search) {
+                    $q->where('name', 'like', "%$search%")
+                        ->orWhere('population', 'like', "%$search%");
+                });
+            });
+
+        $regencies = $query->paginate(10);
+
+        $provinces = Province::all();
+
+        return view('regencies.index', [
+            'regencies' => $regencies,
+            'provinces' => $provinces,
+            'selectedProvince' => $provinceId,
+            'searchQuery' => $search
+        ]);
+    }
+}
