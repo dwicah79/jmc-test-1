dad44f4492980c91ee10be3badfb750e2c3cc3bb
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 02:52:34 +0700

    add button to print pdf population province

--- resources/views/Province/index.blade.php
+++ resources/views/Province/index.blade.php
@@ -1,6 +1,17 @@
 @extends('layout.template')
 @section('title', 'Daftar Profinsi')
 @section('content')
+    <div class="flex w-full justify-end mb-5">
+        <a href="{{ route('reports.provinces') }}"
+            class="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 flex items-center w-1/4 justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
+            </svg>
+            Cetak PDF
+        </a>
+    </div>
     <x-table>
         <x-slot name="thead">
             <tr>
