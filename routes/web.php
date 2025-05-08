8bc2212c66e63773fd581ce94242db45678dc332
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-05-09 00:36:04 +0700

    create route to province

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\ProvinceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('provinces')->group(function () {
+    Route::get('/', [ProvinceController::class, 'index'])->name('provinces.index');
+    Route::post('/', [ProvinceController::class, 'store'])->name('provinces.store');
+    Route::get('/{province}/edit', [ProvinceController::class, 'edit'])->name('provinces.edit');
+    Route::put('/{province}', [ProvinceController::class, 'update'])->name('provinces.update');
+    Route::delete('/{province}', [ProvinceController::class, 'destroy'])->name('provinces.destroy');
 });
